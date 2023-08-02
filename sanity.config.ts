26a2ca87568dc06249e9f001d4da9a0f63487ad8
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-02 12:15:00 +1000

    Update config with basePath

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,19 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
 
 export default defineConfig({
-  name: 'default',
-  title: 'sanity-nextjs-site',
+  name: "default",
+  title: "sanity-nextjs-site",
 
-  projectId: 'ai70dbv7',
-  dataset: 'production',
+  projectId: "ai70dbv7",
+  dataset: "production",
+  basePath: "/studio",
 
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
