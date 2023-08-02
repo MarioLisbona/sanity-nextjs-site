d677c6b08bb0eb5a12c146b3888dcc33c77085ff
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-02 12:12:34 +1000

    Insall sanity and transfer config and schemas to next

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-nextjs-site',
+
+  projectId: 'ai70dbv7',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
